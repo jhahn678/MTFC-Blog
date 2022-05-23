bf4d71af8d13f52329471393f72caa5039b4fcb4
jhahn678 <jhahn678@yahoo.com>
2022-05-23 18:13:50 -0400

    Added context functions for updating user bookmarks and follows

--- src/store/context/auth.js
+++ src/store/context/auth.js
@@ -7,7 +7,13 @@ const initialState = {
     user: {}
 }
 
-const AuthContext = createContext({ authStatus: initialState, setAuthStatus: (state) => state , resetAuthStatus: () => {}})
+const AuthContext = createContext({
+    authStatus: initialState, 
+    setAuthStatus: (state) => state, 
+    resetAuthStatus: () => {},
+    setBookmarks: (arrayOfBookmarks) => {},
+    setFollowing: (arrayOfFollowers) => {}
+})
 
 export const AuthProvider = ({ children }) => {
 
@@ -22,8 +28,32 @@ export const AuthProvider = ({ children }) => {
         toast.info('User signed out')
     }
 
+    const setBookmarks = (array) => {
+        const { user: userData } = authStatus
+        const user = {
+            ...userData,
+            bookmarks: [...array]
+        }
+        setAuthStatus((state) => ({
+            ...state,
+            user
+        }))
+    }
+
+    const setFollowing = (array) => {
+        const { user: userData } = authStatus
+        const user = {
+            ...userData,
+            following: [...array]
+        }
+        setAuthStatus((state) => ({
+            ...state,
+            user
+        }))
+    }
+
     return(
-        <AuthContext.Provider value={{ authStatus, setAuthStatus, resetAuthStatus }}>
+        <AuthContext.Provider value={{ authStatus, setAuthStatus, resetAuthStatus, setBookmarks, setFollowing }}>
             {children}
         </AuthContext.Provider>
     )
