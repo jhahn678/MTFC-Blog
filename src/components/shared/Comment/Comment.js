495ebb7ba8fda72aa9f880c0840b4065bfa6d3e7
jhahn678 <jhahn678@yahoo.com>
2022-05-23 18:15:17 -0400

    Authorization tweaks to comments

--- src/components/shared/Comment/Comment.js
+++ src/components/shared/Comment/Comment.js
@@ -9,52 +9,137 @@ import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import TextField from '@mui/material/TextField'
 import IconButton from '@mui/material/IconButton'
 import Collapse from '@mui/material/Collapse'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { motion } from 'framer-motion'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useAuthContext } from '../../../store/context/auth';
+import { useDeleteCommentMutation, useCreateReplyMutation } from '../../../store/api';
+import { toast } from 'react-toastify';
 
 const Comment = ({ comment }) => {
 
-    const replyRef = useRef()
+    const { authStatus } = useAuthContext()
 
+    const [ deleteComment ] = useDeleteCommentMutation()
+    const [ createReply ] = useCreateReplyMutation()
+
+    const replyRef = useRef()
     const [replyOpen, setReplyOpen] = useState(false)
     const [repliesOpen, setRepliesOpen] = useState(false)
+    const [menuAnchor, setMenuAnchor] = useState(null)
+    const [hideComment, setHideComment] = useState(false)
+
+    const handleMenu = (e) => {
+        if(menuAnchor){
+            setMenuAnchor(null)
+        }else{
+            setMenuAnchor(e.currentTarget)
+        }
+    }
+
+    const handleDeleteComment = async () => {
+        try{
+            const res = await deleteComment({
+                postId: comment.post, 
+                commentId: comment._id
+            }).unwrap()
+            console.log(res)
+            toast.info('Comment deleted')
+        }catch(err){
+            toast.error('Failed to delete comment')
+        }
+    }
+
+    const handleEditComment = async () => {
+        try{
+            console.log('edit')
+            toast.info('Comment edited')
+        }catch(err){
+            toast.error('Failed to edit comment')
+        }
+    }
+
+    const handleReply = async () => {
+        const value = replyRef.current.value
+        if(value.length > 0){
+            try{
+                const res = await createReply({ 
+                    commentId: comment._id,
+                    postId: comment.post, 
+                    body: value
+                }).unwrap()
+                console.log(res)
+                toast.success('Reply sent')
+                replyRef.current.value = '';
+            }catch(err){
+                toast.error('Failed to post reply')
+            }
+        }
+    }
+    
 
     return (
         <div className={classes.container}>
+            <IconButton className={classes.commentMenu} onClick={handleMenu}>
+                <MoreHorizIcon/>
+            </IconButton>
+            <Menu anchorEl={menuAnchor} onClose={handleMenu} open={!!menuAnchor}>
+                <MenuItem onClick={() => setHideComment(h => !h)}>{ hideComment ? 'Unhide' : 'Hide'}</MenuItem>
+                { authStatus.user?._id === comment.user._id && 
+                        <MenuItem onClick={handleEditComment}>Edit</MenuItem>
+                }
+                { authStatus.user?._id === comment.user._id &&
+                    <MenuItem onClick={handleDeleteComment}>Delete</MenuItem> 
+                }
+            </Menu>
             <header className={classes.header}>
                 <Avatar src={comment.user.thumbnail} alt={comment.user.displayName} sx={{ backgroundColor: 'var(--primary)'}}/>
                 <div className={classes.nameAndDate}>
-                    <h3 style={{ margin: 0}}>{comment.user.displayName}</h3>
+                    <h3 style={{ margin: 0}}>{comment.user.displayName || 'Deleted'}</h3>
                     <p style={{ margin: 0, fontSize: '.7em'}}>{formatDateComment(comment.createdAt)}</p>
                 </div>
             </header>
-            <main>
-                <p>{ comment.body }</p>
-            </main>
-            <footer className={classes.footer}>
-                <motion.p className={classes.replies} onClick={() => setRepliesOpen(o => !o)}>
-                    { comment.replies.length > 0 && <>
-                        Replies ({comment.replies.length})
-                        {repliesOpen ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
-                        </>
+            <Collapse in={!hideComment}>
+                <main>
+                    <p>{ comment.body }</p>
+                </main>
+                <footer className={classes.footer}>
+                    <motion.p className={classes.replies} onClick={() => setRepliesOpen(o => !o)}>
+                        { comment.replies?.length > 0 && <>
+                            Replies ({comment.replies.length})
+                            {repliesOpen ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
+                            </>
+                        }
+                    </motion.p>
+                    <Collapse in={repliesOpen} unmountOnExit={true}>
+                        { comment.replies?.map(c => <Comment key={c._id} comment={c}/>) }
+                    </Collapse>
+                    { authStatus.isAuthenticated && comment.user._id &&
+                        <motion.p className={classes.replyToThis}
+                            whileHover={{ x: -5 }}
+                            onClick={() => setReplyOpen(o => !o)}
+                        >Reply to this <ReplyIcon sx={{ marginLeft: 1}}/>
+                        </motion.p>
                     }
-                </motion.p>
-                <Collapse in={repliesOpen} unmountOnExit={true}>
-                    { comment.replies.map(c => <Comment comment={c}/>) }
-                </Collapse>
-                <motion.p className={classes.replyToThis}
-                    whileHover={{ x: -5 }}
-                    onClick={() => setReplyOpen(o => !o)}
-                >Reply to this <ReplyIcon sx={{ marginLeft: 1}}/></motion.p>
-                <Collapse in={replyOpen} unmountOnExit={true}>
-                    <TextField label='Reply'
-                        InputProps={{ endAdornment: <IconButton sx={{padding: 0}}><SendIcon/></IconButton> }}
-                        sx={{ width: '100%', marginTop: 2 }}
-                        multiline={true}
-                        maxRows={3}
-                        inputRef={replyRef}
-                    />
-                </Collapse>
-            </footer>
+                    <Collapse in={replyOpen} unmountOnExit={true}>
+                        <TextField label='Reply'
+                            InputProps={{ 
+                                endAdornment: 
+                                    <IconButton sx={{ padding: 0 }}
+                                        onClick={handleReply}
+                                    >
+                                        <SendIcon/>
+                                    </IconButton> 
+                            }}
+                            sx={{ width: '100%', marginTop: 2 }}
+                            multiline={true}
+                            maxRows={3}
+                            inputRef={replyRef}
+                        />
+                    </Collapse>
+                </footer>
+            </Collapse>
         </div>
     )
 }
