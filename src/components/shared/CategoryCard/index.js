089a19e21ddce867cc94a13d72239bf11e6a9494
jhahn678 <jhahn678@yahoo.com>
2022-05-24 22:07:15 -0400

    Category card component for category page

---
+++ src/components/shared/CategoryCard/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from './CategoryCard.module.css'
+import Paper from '@mui/material/Paper'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const CategoryCard = ({ category, containerClass}) => {
+    return (
+        <Link href={`/category/${category.slug}`}>
+            <Paper className={containerClass}>
+                <Image src={category.thumbnail} alt={category.title} layout='fill' className={classes.image}/>
+                <div className={classes.content}>
+                    <h3 className={classes.title}>{category.title}</h3>
+                </div>
+            </Paper>
+        </Link>
+    )
+}
+
+export default CategoryCard
