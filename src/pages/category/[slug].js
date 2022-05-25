490c9f7dfc31238cd9f9e92530c5765ee8feee69
jhahn678 <jhahn678@yahoo.com>
2022-05-24 22:09:34 -0400

    working on posts by category page

--- src/pages/category/[slug].js
+++ src/pages/category/[slug].js
@@ -1,5 +1,10 @@
 import classes from './Category.module.css'
 import { axios } from '../../utils/axios'
+import PostList from '../../components/shared/PostList'
+import Paper from '@mui/material/Paper'
+import Image from 'next/image'
+import Divider from '@mui/material/Divider'
+import CategoryCard from '../../components/shared/CategoryCard'
 
 export async function getStaticPaths(){
     const { data } = await axios.get('/category')
@@ -12,15 +17,41 @@ export async function getStaticPaths(){
 export async function getStaticProps({ params }){
     const { data: posts } = await axios.get(`/post/category/${params.slug}`)
     const { data: category } = await axios.get(`/category/${params.slug}`)
+    const { data: allCategories } = await axios.get('/category')
+
+    const categories = allCategories.filter(c => c.slug !== params.slug)
+
     return{
-        props: { category, posts}
+        props: { category, categories, posts}
     }
 }
 
-const Category = ({ category, posts }) => {
+const Category = ({ category, categories, posts }) => {
     
     return (
-        <h1 className={classes.title}>{category.title}</h1>
+        <div className={classes.page}>
+            <header>
+                <h1 className={classes.title}>{category.title}</h1>
+            </header>
+            <main className={classes.main}>
+                <PostList containerClass={classes.postList} posts={posts}/>
+                <aside className={classes.aside}>
+                    <h3 className={classes.categoryHeader}>{category.title}
+                        <span style={{ fontSize: '.8em'}}>{posts.length} post(s)</span>
+                    </h3>
+                    <Paper className={classes.imageContainer}>
+                        <Image src={category.thumbnail} alt={`${category.title}`} layout='fill' objectFit='cover'/>
+                    </Paper>
+                    <p className={classes.description}>
+                        <strong style={{ marginRight: 10}}>Description:</strong>
+                        {category.description}
+                    </p>
+                    <Divider sx={{ margin: '1em 0'}}/>
+                    <h3 className={classes.categoryHeader}>Other Categories</h3>
+                    { categories.map(c => <CategoryCard containerClass={classes.categoryCard} category={c}/>) }
+                </aside>
+            </main>
+        </div>
     )
 }
 
