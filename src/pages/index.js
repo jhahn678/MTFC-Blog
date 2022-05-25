22bb143fe79899fa112d06bf97084b51d38079de
jhahn678 <jhahn678@yahoo.com>
2022-05-24 23:45:41 -0400

    Adding categories section to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,47 @@
+import classes from '../styles/home.module.css'
 import { useAuthContext } from '../store/context/auth'
 import { axios } from '../utils/axios'
 import PostSlider from '../components/shared/PostSlider/PostSlider'
+import CategoryCard from '../components/shared/CategoryCard'
+import PostList from '../components/shared/PostList'
+import Link from 'next/link'
+import TextHover from '../components/shared/TextHover'
 
 export async function getStaticProps(){
-  const res = await axios.get('/post')
+  const { data: posts } = await axios.get('/post')
+  const { data: categories } = await axios.get('/category')
   return{
-    props: { posts: res.data }
+    props: { posts, categories }
   }
 }
 
-const HomePage = ({ posts }) => {
+const HomePage = ({ posts, categories }) => {
 
   const { authStatus, resetAuthStatus } = useAuthContext()
 
   return (
-    <div>
+    <main className={classes.page}>
+      <h1 className={classes.header}>Recent Posts</h1>
       <PostSlider posts={posts}/>
-    </div>
+      <h1 className={classes.categoryHeader}>Categories</h1>
+      <section className={classes.categoriesSection}>
+        { categories.map(c => <CategoryCard key={c._id} category={c} containerClass={classes.categoryContainer}/>) }
+      </section>
+      {
+        categories.map(c => 
+          <section className={classes.postSection}>
+            <h2 className={classes.latestInHeader}>
+              <i style={{ marginRight: 10 }}>Latest in </i>
+              <Link href={`/category/${c.slug}`}>
+                <TextHover>
+                  <p className={classes.linkHover}>{c.title}</p>
+                </TextHover>
+              </Link>
+            </h2>
+          </section>
+        )
+      }
+    </main>
   )
 }
 
