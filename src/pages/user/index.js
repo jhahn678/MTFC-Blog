b02f6e4fdb31dfd14d449527f33caf12c6eabeb7
jhahn678 <jhahn678@yahoo.com>
2022-05-26 08:25:50 -0400

    Setting up structure for user page

---
+++ src/pages/user/index.js
@@ -0,0 +1,15 @@
+import { useState, useEffect} from 'react'
+import { useRouter } from 'next/router';
+import Link from 'next/link'
+
+export async function getServerSideProps(){
+
+}
+
+const User = () => {
+    return (
+        <div>User</div>
+    )
+}
+
+export default User;
