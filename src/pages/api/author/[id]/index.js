2b4fe744174c16f58422caa3da44d4ee01d0a0d0
jhahn678 <jhahn678@yahoo.com>
2022-05-28 00:52:28 -0400

    author page & pagination for author posts

--- src/pages/api/author/[id]/index.js
+++ src/pages/api/author/[id]/index.js
@@ -3,14 +3,14 @@ import Author from '../../../../models/author'
 
 export default async function handler(req, res){
 
-    const { id } = req.query;
+    const { id, page=1, limit=8 } = req.query;
 
     await connectMongo()
 
     const author = await Author.findById(id).populate({
         path: 'posts',
         select: '-comments -author',
-        options: { limit: 4 }
+        options: { limit: limit, skip: (limit * ( page - 1 )) }
     })
 
     res.status(200).json(author)
--- src/pages/author/[id].js
+++ src/pages/author/[id].js
@@ -1,4 +1,10 @@
 import { axios } from '../../utils/axios'
+import { useState, useEffect } from 'react'
+import classes from './Author.module.css'
+import { useLazyGetAuthorPostsQuery } from '../../store/api'
+import { toast } from 'react-toastify'
+import Button from '@mui/material/Button'
+import CircularProgress from '@mui/material/CircularProgress'
 
 export async function getStaticPaths(){
     const { data } = await axios.get('/author')
@@ -17,8 +23,49 @@ export async function getStaticProps({ params }){
 
 const Author = ({ author }) => {
 
+    const [posts, setPosts] = useState([])
+    const [pagination, setPagination] = useState({ next: false, page: 1 })
+    const [ getPosts, { isLoading } ] = useLazyGetAuthorPostsQuery()
+
+    useEffect(() => {
+        setPosts(author.posts)
+        if(author.posts.length === 8){
+            setPagination({
+                next: true,
+                page: 1
+            })
+        }
+    },[])
+
+    const loadPosts = async (page=1, limit=8) => {
+        try{
+            const res = await getPosts({ id: author._id, page, limit }).unwrap()
+            setPosts((state) => {
+                return state.concat(res.posts)
+            })
+            setPagination({
+                next: res.posts.length === limit,
+                page: page
+            })
+        }catch(err){
+            toast.error(err)
+        }
+    }
+
     return(
-        <h1>{author.displayName}</h1>
+        <main className={classes.page}>
+            <h1 className={classes.name}>{author.displayName}</h1>
+            { posts.map(p => (
+                <p key={p._id}>{p.title}</p>
+            ))
+            }
+            { pagination.next &&
+                <Button variant='outlined' 
+                    sx={{ padding: '1.5em 4em', alignSelf: 'center'}}
+                    onClick={() => loadPosts(pagination.page + 1)}
+                >Load more</Button>
+            }
+        </main>
     )
 }
 
