f4f8d01bb12d68e8766fe97d990d8a0b38883aa6
jhahn678 <jhahn678@yahoo.com>
2022-05-28 22:56:33 -0400

    API routes for notifications

--- src/pages/api/auth/user/notifications.js
+++ src/pages/api/auth/user/notifications.js
@@ -1,8 +1,10 @@
 import connectMongo from '../../../../utils/connectMongo'
 import User from '../../../../models/user'
+import Post from '../../../../models/post'
+import Comment from '../../../../models/comment'
 import AuthError from '../../../../utils/AuthError'
 import { verifyAuthToken } from '../../../../utils/authToken'
-
+import { createTestNotification } from '../../../../utils/createNotification'
 
 export default async function handler (req, res){
 
@@ -16,7 +18,9 @@ export default async function handler (req, res){
 
     const payload = await verifyAuthToken(token)
 
-    const user = await User.findById(payload._id)
+    if(req.method === 'GET'){
+
+        const user = await User.findById(payload._id)
         .populate({
             path: 'notifications',
             populate: {
@@ -25,5 +29,41 @@ export default async function handler (req, res){
         })
         .select('notifications')
 
-    res.status(200).json(user)
+        return res.status(200).json(user)
+
+    }
+    
+    if(req.method === 'POST'){
+
+        const user = await User.findByIdAndUpdate(payload._id, {
+            $push: { 
+                notifications: createTestNotification() 
+            }
+        }, { new: true }).select('-account.password')
+
+        return res.status(200).json(user)
+
+    }
+
+    if(req.method === 'PATCH'){
+
+        const user = await User.findByIdAndUpdate(payload._id, {
+            $set: { 'notifications.$[].status': 'Read' }
+        }, { new: true }).select('-account.password')
+
+        return res.status(200).json(user)
+
+    }
+
+    if(req.method === 'DELETE'){
+
+        const { notifications } = req.body;
+
+        const user = await User.findByIdAndUpdate(payload._id, {
+            $pull: { notifications: { _id: { $in: notifications } } }
+        }, { new: true }).select('-account.password')
+
+        return res.status(200).json(user)
+
+    }
 }
