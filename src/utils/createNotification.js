f5fc243f1ec1c4f22a32d0daea76363362f393b6
jhahn678 <jhahn678@yahoo.com>
2022-05-29 14:17:44 -0400

    Create notification function

--- src/utils/createNotification.js
+++ src/utils/createNotification.js
@@ -1,7 +1,7 @@
 export const createTestPostNotification = () => {
     return {
         notification_type: 'Post',
-        title: 'TEST: MTFC Team made a new post!',
+        title: 'TEST -- MTFC Team made a new post!',
         avatar: 'https://images.ctfassets.net/fnhljsxx2y84/55CDSo8R6GwoSaVDSNJOgv/6a2f54ed338d5c2918746bbd1351d32a/MTFC-logo.svg',
         post: '628e4ce05c7d447642225b4c'
     }
@@ -10,9 +10,10 @@ export const createTestPostNotification = () => {
 export const createTestReplyNotification = () => {
     return {
         notification_type: 'Reply',
-        title: 'TEST: John Doe replied to your comment',
+        title: 'TEST -- John Doe replied to your comment',
         avatar: 'https://images.ctfassets.net/fnhljsxx2y84/2hECnnGiyiSmvWxX9tBs7d/cddcf6d9f5354ed9e8a7b5c4c159fc53/avatar.jpeg',
-        comment: '62937cf9e6b3c683f5ec3f64'
+        comment: '62937cf9e6b3c683f5ec3f64',
+        post: '628e4ce05c7d447642225b6f'
     }
 }
 
@@ -24,10 +25,11 @@ export const createPostNotification = (postId, authorDisplayName, customTitle) =
     }
 }
 
-export const createReplyNotification = (commentId, userDisplayName, customTitle) => {
+export const createReplyNotification = (commentId, postId, userDisplayName, customTitle) => {
     return {
         notification_type: 'Reply',
         comment: commentId,
+        post: postId,
         title: customTitle || `${userDisplayName} replied to your comment`
     }
 }
