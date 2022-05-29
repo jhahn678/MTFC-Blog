4038990a470cbdef08d514ae45ef862eb04f9e46
jhahn678 <jhahn678@yahoo.com>
2022-05-29 09:33:05 -0400

    Components for different types of notifications

---
+++ src/components/shared/notification/PostNotification/index.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import classes from './index.module.css'
+import { useDeleteNotificationsMutation } from '../../../../store/api'
+import { useAuthContext } from '../../../../store/context/auth'
+import Paper from '@mui/material/Paper'
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton'
+
+const PostNotification = ({ data, setNotifications }) => {
+
+  const { setAuthStatus } = useAuthContext()
+
+  const [ deleteNotifications ] = useDeleteNotificationsMutation()
+
+  const handleDelete = async () => {
+    try{
+        const res = await deleteNotifications([data._id]).unwrap()
+        setAuthStatus((state) => ({ ...state, user: res}))
+        setNotifications(res.notifications)
+    }catch(err){
+        toast.error(err)
+    }
+}
+
+  return (
+    <Paper className={classes.notification}>
+      <IconButton onClick={handleDelete}><DeleteIcon/></IconButton>
+      <h3>{data.title}</h3>
+    </Paper>
+  )
+}
+
+export default PostNotification
