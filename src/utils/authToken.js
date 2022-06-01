c5c5bec9f39adb00fd4c1cac5aa58467b4d11642
jhahn678 <jhahn678@yahoo.com>
2022-06-01 11:18:50 -0400

    Switched to jose JWT package

--- src/utils/authToken.js
+++ src/utils/authToken.js
@@ -1,17 +1,29 @@
-import jwt from 'jsonwebtoken'
 import AuthError from './AuthError'
+import * as jose from 'jose'
 
-export const generateAuthToken = (id, displayName, avatar, expiresIn='30d') => {
-    return jwt.sign({ 
-        _id: id,
-        displayName: displayName, 
-        issuedAt: Date.now(),
-        avatar: avatar
-    }, process.env.JWT_SECRET, { expiresIn: expiresIn })
+export const generateAuthToken = async (_id, displayName, avatar, expiresIn='30d') => {
+
+    const jwk = await jose.importJWK({ kty: 'oct', k: process.env.JWT_SECRET }, 'HS256')
+
+    const token = await new jose.SignJWT({ _id, displayName, avatar })
+        .setProtectedHeader({ alg: 'HS256' })
+        .setIssuedAt()
+        .setExpirationTime(expiresIn)
+        .sign(jwk)
+
+    return token;
+
 }
 
 export const verifyAuthToken = async (token) => {
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET)
-    if(!decoded) throw new AuthError(400, 'Invalid token')
-    return decoded;
+
+    const jwk = await jose.importJWK({ kty: 'oct', k: process.env.JWT_SECRET }, 'HS256')
+
+    try{
+        const { payload } = await jose.jwtVerify(token, jwk)
+        return payload;
+    }catch(err){
+        throw new AuthError(400, 'Invalid token')
+    }
+
 }
