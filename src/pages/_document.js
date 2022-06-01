4454e23fc6f0bf44333c55732489caf1286feaba
jhahn678 <jhahn678@yahoo.com>
2022-06-01 14:23:31 -0400

    Added font optimization

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+          <link href='https://fonts.googleapis.com/css2?family=Lusitana:wght@400;700' rel='stylesheet' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
